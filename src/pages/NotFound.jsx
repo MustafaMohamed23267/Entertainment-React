4ab164b68ecf13fd9c19c4f35c6223a01feb0820
Mustafa Mohamed <120753752+MustafaMohamed23267@users.noreply.github.com>
2026-01-04 21:21:46 +0200

    change Nav

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import bladerunner from '../assets/cinema5.jpg'
 export const NotFound = ()=>
     {
@@ -10,7 +11,7 @@ export const NotFound = ()=>
                         <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">Page not found</h1>
                         <p class="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">Sorry, we couldn’t find the page you’re looking for.</p>
                         <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="/" class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 duration-300">Go back home</a>
+                        <Link to="/" class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 duration-300">Go back home</Link>
                         <a href="#" class="text-sm font-semibold text-white">Contact support <span aria-hidden="true">&rarr;</span></a>
                         </div>
                     </div>
